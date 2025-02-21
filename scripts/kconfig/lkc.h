4ff5aa58d60078ec1ff607e3f0c430d0459551bd
Leah <leah@realfx.rocks>
2025-02-21 19:44:35 +0100

    DRAFT: scripts: kconfig: lkc: Expand

--- scripts/kconfig/lkc.h
+++ scripts/kconfig/lkc.h
@@ -13,7 +13,9 @@
 #else
 # include <features.h>
 # include <libintl.h>
-__attribute_format_arg__(1)
+// __attribute_format_arg__(1)
+// Time to expand !!!
+__attribute__ ((__format_arg__ (1)))
 static inline const char *gettext(const char *txt) { return txt; }
 static inline void textdomain(const char *domainname) {}
 static inline void bindtextdomain(const char *name, const char *dir) {}
